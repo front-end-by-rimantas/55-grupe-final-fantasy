19777031cd64ac297f8b0e73ee6030218d244221
Rimantas <belauzas@gmail.com>
2025-08-11 11:11:26 +0300

    Up

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 import { postRegister } from './src/api/public/postRegister.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     return res.json({
