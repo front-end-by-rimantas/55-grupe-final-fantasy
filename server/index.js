a725f96c6657c67d57af65c549ae62e000e40988
Rimantas <belauzas@gmail.com>
2025-08-12 11:09:15 +0300

    Auto-login WIP

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import { postRegister } from './src/api/public/postRegister.js';
 import { postLogin } from './src/api/public/postLogin.js';
@@ -7,7 +8,12 @@ import { postLogin } from './src/api/public/postLogin.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(helmet());
+app.use(cors({
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://localhost:5520',
+}));
 
 app.get('/', (req, res) => {
     return res.json({
