9ee3f76dea095210e1d373f45b10e882dfb0f374
Rimantas <belauzas@gmail.com>
2025-08-18 11:11:10 +0300

    Nu tai va

--- client/src/pages/public/CategoryInner.jsx
+++ client/src/pages/public/CategoryInner.jsx
@@ -3,9 +3,11 @@ import { MovieCard } from '../../components/MovieCard';
 import { useContext } from 'react';
 import { CategoriesContext } from '../../context/categories/CategoriesContext';
 import { useParams } from 'react-router';
+import { MoviesContext } from '../../context/movies/MoviesContext';
 
 export function CategoryInnerPage() {
     const { publicCategories } = useContext(CategoriesContext);
+    const { publicMovies } = useContext(MoviesContext);
     const { category } = useParams();
 
     const categoryData = publicCategories.find(c => c.url_slug === category);
@@ -24,7 +26,7 @@ export function CategoryInnerPage() {
         );
     }
 
-    const moviesData = [{}, {}];
+    const moviesData = publicMovies.filter(m => m.category_id === categoryData.id);
 
     return (
         <main className='min-page-height'>
@@ -32,7 +34,11 @@ export function CategoryInnerPage() {
 
             <div className="container">
                 <div className="row">
-                    {moviesData.map((movie, index) => <MovieCard key={index} movie={movie} />)}
+                    {
+                        moviesData.length
+                            ? moviesData.map((movie, index) => <MovieCard key={index} movie={movie} />)
+                            : <div className='col-12 alert alert-warning'>Panašu, jog šioje kategorijoje šiuo metu nėra jokių filmų.</div>
+                    }
                 </div>
             </div>
         </main>
